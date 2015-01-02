e423b17f2f6ad2ce9bd38a29539ecd3279f54643
Nils Guillermin <nils.guillermin@gmail.com>
2015-01-02 16:06:31 -0500

    Add Windows serial port enumerator

--- automator.py
+++ automator.py
@@ -114,7 +114,7 @@ class SpectrumAnalyzer:
 		self.serial.close()
 		
     def getfft(self):
-		"Gets fft of trace: 0 for spectrum, 1 for PSD"
+	"Gets fft of trace: 0 for spectrum, 1 for PSD"
 
         input = "SPEC?" + str(self.trace) + "0,154"
 		# From http://stackoverflow.com/questions/676172/full-examples-of-using-pyserial-package
@@ -263,6 +263,29 @@ def manual():
 # 3.90625
 # #### Test######
 #testy = {"1.0": "1E-4, 2u", "2.0": "200E-4, 20u", "4.0": "2050E-4, 200u"}
+
+# http://eli.thegreenplace.net/2009/07/31/listing-all-serial-ports-on-windows-with-python/
+from serial.tools import list_ports
+import _winreg as winreg
+import itertools
+
+def enumerate_serial_ports():
+    """ Uses the Win32 registry to return an
+        iterator of serial (COM) ports
+        existing on this computer.
+    """
+    path = 'HARDWARE\\DEVICEMAP\\SERIALCOMM'
+    try:
+        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, path)
+    except WindowsError:
+        raise IterationError
+
+    for i in itertools.count():
+        try:
+            val = winreg.EnumValue(key, i)
+            yield str(val[1])
+        except EnvironmentError:
+            break
 
 if __name__ == '__main__':
     spec = SpectrumAnalyzer('COM4')
