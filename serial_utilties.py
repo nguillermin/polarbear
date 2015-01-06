312bf74f2e652da793c52ef817acc2e41e9cef15
Nils Guillermin <nils.guillermin@gmail.com>
2015-01-05 21:14:13 -0500

    split windows-only serial utilities

---
+++ serial_utilties.py
@@ -0,0 +1,22 @@
+# http://eli.thegreenplace.net/2009/07/31/listing-all-serial-ports-on-windows-with-python/
+from serial.tools import list_ports
+import _winreg as winreg
+import itertools
+
+def enumerate_serial_ports():
+    """ Uses the Win32 registry to return an
+        iterator of serial (COM) ports
+        existing on this computer.
+    """
+    path = 'HARDWARE\\DEVICEMAP\\SERIALCOMM'
+    try:
+        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, path)
+    except WindowsError:
+        raise IterationError
+
+    for i in itertools.count():
+        try:
+            val = winreg.EnumValue(key, i)
+            yield str(val[1])
+        except EnvironmentError:
+            break
