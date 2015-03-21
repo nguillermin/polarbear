171e0287e7a2f84f83a888c3b361877cf002d9ef
Nils Guillermin <nils.guillermin@gmail.com>
2015-03-21 13:55:53 -0400

    test stub for save_multiple

--- test_automator.py
+++ test_automator.py
@@ -12,6 +12,13 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertEqual((negative,[]),automator.split_voltages(negative))
         straddle = range(-10,10)
         self.assertEqual(([-10,-9,-8,-7,-6,-5,-4,-3,-2,-1],[0,1,2,3,4,5,6,7,8,9]),automator.split_voltages(straddle))
+        
+    def test_save_multiple(self):
+        """Ensures multiple data sets are written in columnar csv format"""
+        dataset_1 = {k: [200, 1e-5] for k in range(-4,4)}
+        dataset_2 = {k: [20, 1.5e-5] for k in range(-3,5)}
+        dataset_3 = {k: [2000, 2e-5] for k in range(-4,5)}
+        
 
 if __name__=='__main__':
     unittest.main()
